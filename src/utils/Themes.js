a8da16e50b2893e95e813592a4fdb9e70f47a96b
MostafaDVS <150343140+MostafaDVS@users.noreply.github.com>
2023-11-15 11:34:07 -0800

    adding files

---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+    bg:"#001814",
+    bgLight: "#001814",
+    primary:"#00b494",
+    text_primary:"#ffffff",
+    text_secondary:"#ffffff",
+    card:"#00130f",
+    card_light: '#00130f',
+    button:"#22c5a8",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    bg:"#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary:"#be1adb",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    button:"#22c5a8",
+}
